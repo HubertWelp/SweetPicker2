682bdc68cd575219d1d36c223479887201d71b4b
naogeo2 <ITL.NAO.2@LAPTOP-O9NURME8.de>
2018-01-26 12:25:11 +0100

    Arm

--- Bewegung.py
+++ Bewegung.py
@@ -10,7 +10,8 @@ class Bewegung:
         def greifeObjektLinks(self):
                 print('greifeObjektLinks')
                 self.postureProxy.goToPosture("Crouch", 0.5)
-				bla=3
+		bla=000
+
 				
         def greifeObjektRechts(self):
                 print('greifeObjektRechts')
